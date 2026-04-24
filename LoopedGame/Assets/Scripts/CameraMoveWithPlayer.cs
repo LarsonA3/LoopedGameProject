0755d47e65a021e6d5af56edcf5baa07235dee17
LarsonA3 <glitchy721@gmail.com>
2026-04-24 13:43:08 -0500

    Added 2.5d topdown camera prefab+Script

---
+++ LoopedGame/Assets/Scripts/CameraMoveWithPlayer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraMoveWithPlayer : MonoBehaviour
+{
+    public GameObject plrCapsule;
+    public float height = 10.0f; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(plrCapsule.transform.position.x, height, plrCapsule.transform.position.z);
+    }
+}
