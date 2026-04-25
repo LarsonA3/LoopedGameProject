85e20ddbf5cbb06a036862ef364fabdc4cae21cc
LarsonA3 <glitchy721@gmail.com>
2026-04-25 12:34:16 -0500

    created zone 1 manager
    also changed heirarchy folders

---
+++ LoopedGame/Assets/Scripts/Zone1/Zone1Manager.cs
@@ -0,0 +1,21 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SubsystemsImplementation.Extensions;
+
+public class Zone1Manager : MonoBehaviour
+{
+
+    private int currentRoom;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentRoom = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
